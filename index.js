86a42b755781bf6f62f7ca7a6c90f648fbf0ff25
Stanislav Kotelnykov <crock@Crock-MacBook-Pro.local>
2025-03-23 00:18:51 +0200

    do fibonacci task-2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const inputNumber = document.querySelector(".number");
 const resultNumber = document.querySelector(".result");
 
-const x = 8;
-const y = 21;
+inputNumber.addEventListener("input", handleInput);
 
-inputNumber.value = x;
-resultNumber.textContent = y;
+function handleInput(e) {
+  const inputedString = e.target.value;
+
+  if (inputedString === "") {
+    resultNumber.textContent = "";
+  } else if (Number(inputedString) === "NaN") {
+    alert('Fill please only number from "0" till "20"');
+    inputNumber.value = "";
+  } else {
+    const x = Number(inputedString);
+    resultNumber.textContent = getFibonacciNumber(x);
+  }
+}
+
+function getFibonacciNumber(x) {
+  if (x <= 1) return x;
+
+  let a = 0,
+    y = 1;
+  let temporary;
+  for (let i = 2; i <= x; i++) {
+    temporary = y;
+    y = a + y;
+    a = temporary;
+  }
+  return y;
+}
